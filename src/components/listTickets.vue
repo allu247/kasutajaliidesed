391005c5fc0cd93dda6f112140f634523e2359a3
Martin <martin.verrev@gmail.com>
2019-04-30 15:14:58 +0300

    Ticket List Wip

--- src/components/listTickets.vue
+++ src/components/listTickets.vue
@@ -1,64 +1,61 @@
-<template>
-    <div class="row align-items-start border border-black pb-3 pt-3">
-        <div class="col-sm-12 col-md-6 col-lg-3">
-            <p style = "text-align:center;">
-                Sihtkoht: {{tickets[0].destination}}
-            </p>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-3" >
-            <p style = "text-align:center;">
-                Hind: {{tickets[0].price}}
-            </p>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-3" >
-            <p style = "text-align:center;">
-                Lennuliin: {{tickets[0].airline}}
-            </p>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-3" >
-            <p style = "text-align:center;">
-                Väljumine: {{tickets[0].leaveDate}}
-            </p>
-            <a class="btn btn-lg btn-block btn-outline-primary" @click="goToCart">Lisa korvi</a>
-        </div>
 
+
+<template>
+    <div v-for="(ticket, index) in tickets">
+        <div class="row align-items-start border border-black pb-3 pt-3">
+            <div class="col-sm-12 col-md-6 col-lg-3">
+                <p style = "text-align:center;">
+                    Sihtkoht: {{tickets[0].destination}}
+                </p>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3" >
+                <p style = "text-align:center;">
+                    Hind: {{tickets[0].price}}
+                </p>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3" >
+                <p style = "text-align:center;">
+                    Lennuliin: {{tickets[0].airline}}
+                </p>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3" >
+                <p style = "text-align:center;">
+                    Väljumine: {{tickets[0].leaveDate}}
+                </p>
+                <a class="btn btn-lg btn-block btn-outline-primary" @click="goToCart">Lisa korvi</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    /* eslint-disable no-console */
 
-
+    //https://vuejs.org/v2/guide/list.html
 
     export default {
         name: "listTickets",
 
         data: function () {
             return {
-                    tickets1: [{
-
-                        destination: "Jaapan/Tokyo", price:"300 eurot", airline: "Ryan Air", leaveDate: "Väljumine: 04.04.2019"
-
-                    }],
-
-                    tickets: ""
-
+                tickets: []
             }
         },
 
         mounted() {
-            fetch('./json/ticket.json').then(
-                function (response) {
-                    console.log(response);
+            fetch('./json/ticket.json')
+                .then(response => {
                     return response.json();
-                }
-            ).then(function (json) {
-
-                this.tickets = json;
-                console.log(this.tickets)
-            })
+                })
+                .then(json => {
+                    console.log(json);
+                    this.tickets = json;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
         }
         ,
-
 
 
         methods: {
